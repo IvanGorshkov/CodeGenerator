23f484781eaf287d636670dda192905a0a0e9491
Ivan Gorshkov <ivanagorshkov160@gmail.com>
2020-09-23 17:28:20 +0300

    Initial Commit

---
+++ CodeGenerator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CodeGenerator
+//
+//  Created by Ivan Gorshkov on 23.09.2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
